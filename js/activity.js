6577aa3fd2bb865c4922ecbeb1d8c9e6e52e7728
Gonzalo Odiard <godiard@gmail.com>
2014-09-29 15:20:13 -0300

    Highlight the user selection

--- js/activity.js
+++ js/activity.js
@@ -62,8 +62,20 @@ define(function (require) {
 
         puzzleString = wordfind.print(puzzle);
         var stage;
+        var cell_size = 40;
+        var margin_x = 20;
+        var margin_y = 20;
+
+        var cell_pressed = null;
+        var select_word_line = null;
 
         init();
+
+        function get_cell(x, y) {
+            cell_x = parseInt((x + cell_size / 2) / cell_size);
+            cell_y = parseInt((y + cell_size / 2) / cell_size);
+            return [cell_x, cell_y];
+        }
 
         function init(){
             canvas = doc.getElementById("testCanvas");
@@ -71,13 +83,21 @@ define(function (require) {
 
             stage = new createjs.Stage(canvas);
 
-            margin_x = 20;
-            margin_y = 20;
-            cell_size = 40;
+            // Enable touch interactions if supported on the current device:
+            createjs.Touch.enable(stage);
+            stage.enableDOMEvents(true);
+
+            select_word_line = new createjs.Shape();
 
             var container = new createjs.Container();
             container.x = margin_x;
             container.y = margin_y;
+
+            var background = new createjs.Shape();
+            background.graphics.beginFill(
+                "#eeeeee").drawRect(
+                0, 0, cell_size * puzzle.length, cell_size * puzzle.length);
+            container.addChild(background);
 
             for (var i = 0, height = puzzle.length; i < height; i++) {
                 row = puzzle[i];
@@ -105,8 +125,48 @@ define(function (require) {
             }
             container.cache(0, 0, cell_size * puzzle.length,
                             cell_size * puzzle.length);
+            stage.on("stagemousedown", mousedown_cb);
+            stage.on("stagemousemove", mouseover_cb);
+            stage.on("stagemouseup", mouseup_cb);
             stage.addChild(container);
 
+            stage.addChild(select_word_line);
+
+            stage.update();
+        }
+
+        function mousedown_cb(event) {
+            cell = get_cell(event.stageX, event.stageY);
+            cell_x = cell[0];
+            cell_y = cell[1];
+            cell_pressed = [cell_x, cell_y];
+        }
+
+        function mouseup_cb(event) {
+            cell_pressed = null;
+        }
+
+        function mouseover_cb(event) {
+            if (cell_pressed == null) {
+                return;
+            }
+
+            end_cell = get_cell(event.stageX, event.stageY);
+            end_cell_x = end_cell[0];
+            end_cell_y = end_cell[1];
+
+            start_cell_x = cell_pressed[0];
+            start_cell_y = cell_pressed[1];
+
+            select_word_line.graphics.clear();
+            select_word_line.graphics.beginStroke(
+                createjs.Graphics.getRGB(0xFF00FF, 0.2));
+            select_word_line.graphics.setStrokeStyle(cell_size, "round");
+            select_word_line.graphics.moveTo(start_cell_x * cell_size,
+                                    start_cell_y * cell_size);
+            select_word_line.graphics.lineTo(end_cell_x * cell_size,
+                                    end_cell_y * cell_size);
+            select_word_line.graphics.endStroke();
             stage.update();
         }
 
